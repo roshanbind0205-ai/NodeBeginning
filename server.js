dc177d0e8f3d6be4a427535a7bea94db0089dcd7
Roshan Kumar <roshanbind0205@gmail.com>
2026-08-19 13:18:06 +0530

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Varanasi Software Junction');
+  res.end('Hello');
 });
 
 server.listen(port, hostname, () => {
