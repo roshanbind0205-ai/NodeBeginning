16fad475fa0f5a4f36d7732bc335660586eb8bf2
Roshan Kumar <roshanbind0205@gmail.com>
2026-08-20 12:43:42 +0530

    Get And Post

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const  express  = require("express");
+const express = require("express");
 const server = express();
-const hostname = "127.0.0.1";
+const hostname  = "127.0.0.1";
 const port = 3000;
+
+server.use(express.urlencoded({extended: true}));
+
 server.get("/",(req,res)=>{
-  const queryObject = req.query;
-  res.setHeader("Content-Type","application/json");
-  res.send(JSON.stringify(queryObject));
+  res.send(`
+    <form method="POST" action="/submit">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name"><br><br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"><br><br>
+    <button type="submit">Submit</button>
+    </form>
+    `);
 });
 
-server.listen(port,hostname,()=>{
+server.post("/submit",(req,res)=>{
+  const {name, email}= req.body;
+  res.send(`Name: ${name},Email: ${email}`);
+
+});
+
+server.listen(port, hostname, ()=>{
   console.log(`Server running at http://${hostname}:${port}/`);
 
 });
