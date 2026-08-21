9b52e5c8f10c46b195537daa8641b435430fa818
Roshan Kumar <roshanbind0205@gmail.com>
2026-08-21 15:13:12 +0530

    check box

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const server = express();
-
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -8,53 +7,25 @@ server.use(express.urlencoded({ extended: true }));
 
 server.get("/", (req, res) => {
   res.send(`
-     <form method="POST" action="/submit">   
-
-     <label for="number">Num1:</label>
-     <input type="number" id="number" name="number1"><br><br>
-     <label for="number">Num2:</label>
-     <input type="number" id="number" name="number2"><br><br>
-    <select name="option">
-    <option value="add">Add</option>
-    <option  value="sub">Sub</option>
-    <option value="multi">Multi</option>
-    <option value="div">Div</option>
-    </select>
-
+    <form method="POST" action="/submit">
+      <label for="a">A:</label>
+      <input type="text" id="a" name="a"><br><br>
+      <label for="b">B:</label>
+      <input type="text" id="b" name="b"><br><br>
+      <label for="option">Add/Sub</label><input value="add" type="checkbox" id="option" name="option"/><br><br>
       <button type="submit">Submit</button>
-
-     </form>
-   `);
+    </form>
+  `);
 });
 
 server.post("/submit", (req, res) => {
-  let { number1, number2, option } = req.body;
-
-  console.log(number1, number2, option);
-  let result = 0;
-  number1 = Number(number1);
-  number2 = Number(number2);
-  if (option == "add") {
-    result = number1 + number2;
-  }
-  if (option == "sub") {
-    result = number1 - number2;
-  }
-  if(option == "multi"){
-    result=number1 * number2;
-  }
-  if(option == "div"){
-    result = number1 / number2;
-  }
-  const sum = number1 * 1 + number2 * 1;
-  const output = {
-    "a": number1,
-    "b": number2,
-    "Answer": result
-
-  };
-  res.send(JSON.stringify(output));
-  //  res.send(req.body);
+  let { a, b,option } = req.body;
+  a=Number(a);
+  b=Number(b);
+  let result=a-b;
+  if(option=="add")
+    result=a+b;
+  res.send(`a: ${a}, b: ${b}, option:${option}, Result: ${result}`);
 });
 
 server.listen(port, hostname, () => {
